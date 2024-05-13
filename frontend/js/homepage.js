fd6b628c0e199e71676d9282d74485ff1ec9d55f
Pavel <ralibs4@gmail.com>
2024-05-13 22:11:41 +0300

    Стили для слайдера.

--- frontend/js/homepage.js
+++ frontend/js/homepage.js
@@ -1,75 +1,84 @@
-let count = 0;
-let buttons = document.querySelector('.reviews');
+if(document.body.querySelector(".reviews")) {
+    let count = 0;
+    let buttons = document.querySelector('.reviews');
 
+    $(document).ready(function() {
+        $('.reviews').click(function(event) {
+            if(event.target.className == 'reviews__previous') {
+                let oldCount = count;
+                count--;
+                if(count < 0) count = 9;
+                let currentReview = document.querySelector(`.review-${count}`);
+                let lastReview = document.querySelector(`.review-${oldCount}`);
+                $(`.review-${oldCount}`).animate({opacity: 'hide'}, 'slow');
+                setTimeout(() =>  lastReview.classList.add('invis') , 100);
+                setTimeout(() =>  currentReview.classList.remove('invis') ,100);
+                $(`.review-${count}`).animate({opacity: 'show'}, 'slow');
 
-$(document).ready(function() {
-    $('.reviews').click(function(event) {
-        if(event.target.className == 'reviews__previous') {
-            let oldCount = count;
-            count--;
-            if(count < 0) count = 9;
-            let currentReview = document.querySelector(`.review-${count}`);
-            let lastReview = document.querySelector(`.review-${oldCount}`);
-            $(`.review-${oldCount}`).animate({opacity: 'hide'}, 'slow');
-            setTimeout(() =>  lastReview.classList.add('invis') , 100);
-            setTimeout(() =>  currentReview.classList.remove('invis') ,100);
-            $(`.review-${count}`).animate({opacity: 'show'}, 'slow');
-
-          
-        };
-        if(event.target.className == 'reviews__next') {
-            let oldCount = count;
-            count++;
-            if(count > 9) count = 0;
-            let currentReview = document.querySelector(`.review-${count}`);
-            let lastReview = document.querySelector(`.review-${oldCount}`);
-            $(`.review-${oldCount}`).animate({opacity: 'hide'}, 'slow');
-            setTimeout(() => lastReview.classList.add('invis'), 100);
-            setTimeout(() => currentReview.classList.remove('invis'), 100);
-            $(`.review-${count}`).animate({opacity: 'show'}, 'slow');
             
-            
-        }
-})
+            };
+            if(event.target.className == 'reviews__next') {
+                let oldCount = count;
+                count++;
+                if(count > 9) count = 0;
+                let currentReview = document.querySelector(`.review-${count}`);
+                let lastReview = document.querySelector(`.review-${oldCount}`);
+                $(`.review-${oldCount}`).animate({opacity: 'hide'}, 'slow');
+                setTimeout(() => lastReview.classList.add('invis'), 100);
+                setTimeout(() => currentReview.classList.remove('invis'), 100);
+                $(`.review-${count}`).animate({opacity: 'show'}, 'slow');
+                
+                
+            }
+    })
 
-})
-
-
-
-
-
-
-// SLICK SLIDER
-
-function calcScroll() {
-    let div = document.createElement('div');
-
-    div.style.width = '50px';
-    div.style.height = '50px';
-    div.style.overflowY = 'scroll';
-    div.style.visibility = 'hidden';
-
-    document.body.appendChild(div);
-    let scrollWidth = div.offsetWidth - div.clientWidth;
-    div.remove();
-
-    return scrollWidth;
-}
-
-
-if(document.documentElement.clientWidth <= 574) {
-    $('.multiple-items').slick({
-        infinite: true,
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        arrows: false,
-        dots: true
-    });
-    console.log($('.multiple-items').length);
-}
+    })
 
 
 
+    // SLICK SLIDER
+
+    function calcScroll() {
+        let div = document.createElement('div');
+
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
+    }
+
+
+    if(document.documentElement.clientWidth <= 574 && document.title == 'Meridian|Homepage') {
+        $('.multiple-items').slick({
+            infinite: true,
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            arrows: false,
+            dots: true
+        });
+    };
 
 
 
+
+
+    console.log(document.title);
+
+    if(localStorage.getItem('itemCount')) {
+        let itemsCount = document.body.querySelector('.nav-header__count');
+        itemsCount.innerHTML = localStorage.getItem('itemCount');
+    }
+
+
+};
+
+
+
+
+     
